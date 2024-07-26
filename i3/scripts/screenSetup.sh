2fe24aa78b58820a50f99f1a905772171ee65acd
W1ck29 <midorek95@gmail.com>
2024-07-26 02:03:05 +0200

    idk

--- i3/scripts/screenSetup.sh
+++ i3/scripts/screenSetup.sh
@@ -1,2 +1,4 @@
+xrandr --newmode "2560x1080_75.00"  294.00  2560 2744 3016 3472  1080 1083 1093 1130 -hsync +vsync &
+xrandr --verbose --addmode HDMI-1 "2560x1080_75.00" & 
 xrandr --output HDMI-1 --mode "2560x1080_75.00" &
 xrandr --output DP-1 --left-of HDMI-1 &
